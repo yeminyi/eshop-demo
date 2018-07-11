c477ae4d95af9a4763de7cc6efdaf93a40789504
Amy <yeminyi@outlook.com>
2018-07-11 23:47:33 +1000

    rating calculate

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -24,6 +24,8 @@ export class ProductDetailComponent implements OnInit {
   {
     let comment = new Comment(0,this.product.id,new Date().toISOString(),'userTest',this.newRating,this.newComment);
     this.comments.unshift(comment);
+    let sum=this.comments.reduce((sum,comment)=>sum+comment.rating,0);
+    this.product.rating=sum/this.comments.length;
     this.newRating=5;
     this.newComment=null;
     this.isHiddenComment=true;
