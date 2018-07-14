18e956d0697690ff294b7c041dcbfe51e8e6485b
Amy <yeminyi@outlook.com>
2018-07-14 23:06:24 +1000

    Rex for validation

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -19,9 +19,9 @@ export class SearchComponent implements OnInit {
     
     this.formModel=this.formBuilder.group({   
       title:['',Validators.minLength(3)],
-      price:[null,this.positiveNumberValidator],
-      category:['-1'],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      // price:[null,this.positiveNumberValidator],
+      price:[null,Validators.pattern('^[0-9]+(.[0-9]{2})?$')],
+      category:['-1']
     });
    }
 
@@ -29,39 +29,41 @@ export class SearchComponent implements OnInit {
     this.categories=this.productService.getAllCategories();
     this.categories.unshift(this.selectCategory);
   }
-  get name() { return this.formModel.get('name'); }
+
   get f()
   {
     return this.formModel.controls;
   }
-  positiveNumberValidator(control:FormControl):any{
-    console.log(control.value);
+  // positiveNumberValidator(control:FormControl):any{
+  //   console.log(control.value);
     
-    if (!control.value){
-      return null;
-    }
-    let price =parseInt(control.value);
-    if(price >0){
-      return null;
-    }else{
-      return {
-        positiveNumber:true
-      }
-    }
-  }
+  //   if (!control.value){
+  //     return null;
+  //   }
+  //   let price =parseInt(control.value);
+  //   console.log(price);
+    
+  //   if(price >0){
+  //     return null;
+  //   }else{
+  //     return {
+  //       positiveNumber:true
+  //     }
+  //   }
+  // }
   onSearch(){
-
     this.submitted = true;
-
     // stop here if form is invalid
     if (this.formModel.invalid) {
+      console.log(this.formModel.invalid);
+      console.log(this.formModel.controls);
       console.log(this.formModel.value);
         return;
     }
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.formModel.value))
   }
   selectBtn(category){
-    console.log(category);
+    // console.log(category);
     this.selectCategory=category;
 
   }
